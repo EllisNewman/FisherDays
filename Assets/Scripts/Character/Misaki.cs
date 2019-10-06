13779979148944f2d561129e95a61760adb01f15
くれの <ellisnewmanem@outlook.com>
2019-10-06 21:21:42 +0800

    Initial commit

---
+++ Assets/Scripts/Character/Misaki.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Misaki : MonoBehaviour
+{
+    public GameObject fishString1;
+    public GameObject fishString2;
+    
+    private Animator charaAnimator;
+    
+    void Start()
+    {
+        charaAnimator = GetComponent<Animator>();
+        fishString1 = GameObject.Find("string");
+        fishString2 = GameObject.Find("string_with_bait");
+        fishString1.SetActive(false);
+        fishString2.SetActive(false);
+    }
+    
+    void Update()
+    {
+
+    }
+
+    public void SetCurrentSpriteByIndex(int index)
+    {
+        if(index == 2)
+        {
+            fishString1.SetActive(true);
+        }
+        else
+        {
+            fishString1.SetActive(false);
+        }
+        if (index == 3)
+        {
+            fishString2.SetActive(true);
+        }
+        else
+        {
+            fishString2.SetActive(false);
+        }
+    }
+}
