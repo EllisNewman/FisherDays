fe8268c4f3f1c17beb94922647b263fba436ffce
くれの <ellisnewmanem@outlook.com>
2019-10-09 22:37:32 +0800

    edit animation. adjust file

--- Assets/Scripts/Base/RuleManager.cs
+++ Assets/Scripts/Base/RuleManager.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class RuleManager : MonoBehaviour
 {
     public GameObject[] idolList;
+    public GameObject gadgetPrefab;
+
+    private List<GameObject> idolAnim;
+    private float countDownTimer;
 
     // Start is called before the first frame update
     void Start()
     {
+        idolAnim = new List<GameObject>();
         RandomBackground();
         RandomIdolAnimation();
+        countDownTimer = Random.Range(10f, 60f);
     }
     
+    void Update()
+    {
+        if(countDownTimer > float.Epsilon)
+        {
+            countDownTimer = countDownTimer - Time.deltaTime;
+        }
+        else
+        {
+            countDownTimer = Random.Range(10f, 60f);
+            PickGadgetAnimate();
+        }
+    }
 
     void RandomBackground()
     {
@@ -23,10 +41,35 @@ public class RuleManager : MonoBehaviour
 
     void RandomIdolAnimation()
     {
-        foreach(var idol in idolList)
+        foreach (var idol in idolList)
         {
-            Animator animator = idol.GetComponentInChildren<Animator>();
-            animator.Play("Idol", 0, Random.Range(0f, 1f));
+            int coin = Random.Range(1, 3);
+            if (coin > 1)
+            {
+                idol.gameObject.SetActive(false);
+            }
+            else
+            {
+                Animator animator = idol.GetComponentInChildren<Animator>();
+                animator.Play("Idol", 0, Random.Range(0f, 1f));
+                idolAnim.Add(idol.gameObject);
+            }
         }
     }
+
+    void PickGadgetAnimate()
+    {
+        GameObject chara = idolAnim[Random.Range(0, idolAnim.Count)];
+        GameObject gadget = Instantiate(gadgetPrefab, chara.transform.position, new Quaternion(0, 0, 0, 0));
+        gadget.transform.SetParent(chara.transform);
+        gadget.GetComponentInChildren<Animator>().Play("Gadget" + Random.Range(1,4));
+        StartCoroutine(KillGadget(gadget));
+    }
+
+    IEnumerator KillGadget(GameObject go)
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(go);
+    }
+
 }
